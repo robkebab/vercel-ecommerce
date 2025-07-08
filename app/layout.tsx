eb230ad2b15047babb648196875dc04039cc61f3
robkebab <58490986+robkebab@users.noreply.github.com>
2025-07-07 21:53:56 -0500

    enable speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,13 @@
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import { CartProvider } from 'components/cart/cart-context';
 import { Navbar } from 'components/layout/navbar';
 import { WelcomeToast } from 'components/welcome-toast';
 import { GeistSans } from 'geist/font/sans';
 import { getCart } from 'lib/shopify';
+import { baseUrl } from 'lib/utils';
 import { ReactNode } from 'react';
 import { Toaster } from 'sonner';
 import './globals.css';
-import { baseUrl } from 'lib/utils';
 
 const { SITE_NAME } = process.env;
 
@@ -41,6 +42,7 @@ export default async function RootLayout({
             <WelcomeToast />
           </main>
         </CartProvider>
+        <SpeedInsights />
       </body>
     </html>
   );
